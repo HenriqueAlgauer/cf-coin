15f8a90a3fe9c66e09224418eca31ce1d647bd19
henriqueAlgauer <algauer.henrique@gmail.com>
2025-02-02 14:58:41 -0300

    alterado profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getUserProfile, updateUserEmail } from "../api/api";
+import { getUserProfile } from "../api/api";
 import AdminMenu from "../components/Admin/AdminMenu";
 import Menu from "../components/Menu";
 
@@ -16,32 +16,15 @@ function Profile() {
     coins: 0,
   });
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [newEmail, setNewEmail] = useState("");
-
   useEffect(() => {
     async function fetchUserData() {
       if (!userId) return;
 
       const data = await getUserProfile(userId);
       setUserData(data);
-      setNewEmail(data.email);
     }
     fetchUserData();
   }, [userId]);
-
-  const handleSave = async () => {
-    if (newEmail === userData.email) {
-      setIsEditing(false);
-      return;
-    }
-
-    const updatedUser = await updateUserEmail(userId, { email: newEmail });
-    if (updatedUser) {
-      setUserData(updatedUser);
-      setIsEditing(false);
-    }
-  };
 
   return (
     <div className="flex flex-col bg-gray-900 gap-[80px] h-screen">
@@ -57,7 +40,7 @@ function Profile() {
                 type="text"
                 value={userData.name}
                 disabled
-                className="p-2 w-full bg-gray-700 rounded outline-none cursor-not-allowed"
+                className="p-2 w-full bg-gray-700 rounded outline-none "
               />
             </div>
 
@@ -67,7 +50,7 @@ function Profile() {
                 type="text"
                 value={userData.department}
                 disabled
-                className="p-2 w-full bg-gray-700 rounded outline-none cursor-not-allowed"
+                className="p-2 w-full bg-gray-700 rounded outline-none "
               />
             </div>
 
@@ -77,53 +60,18 @@ function Profile() {
                 type="text"
                 value={userData.coins}
                 disabled
-                className="p-2 w-full bg-gray-700 rounded outline-none cursor-not-allowed"
+                className="p-2 w-full bg-gray-700 rounded outline-none "
               />
             </div>
 
             <div>
               <label className="block text-gray-400">Email</label>
-              {isEditing ? (
-                <input
-                  type="email"
-                  value={newEmail}
-                  onChange={(e) => setNewEmail(e.target.value)}
-                  className="p-2 w-full bg-gray-700 rounded outline-none"
-                />
-              ) : (
-                <input
-                  type="text"
-                  value={userData.email}
-                  disabled
-                  className="p-2 w-full bg-gray-700 rounded outline-none cursor-not-allowed"
-                />
-              )}
-            </div>
-
-            <div className="flex justify-between mt-4">
-              {isEditing ? (
-                <>
-                  <button
-                    className="bg-green-500 px-4 py-2 rounded"
-                    onClick={handleSave}
-                  >
-                    Salvar
-                  </button>
-                  <button
-                    className="bg-gray-500 px-4 py-2 rounded"
-                    onClick={() => setIsEditing(false)}
-                  >
-                    Cancelar
-                  </button>
-                </>
-              ) : (
-                <button
-                  className="bg-blue-500 px-4 py-2 rounded"
-                  onClick={() => setIsEditing(true)}
-                >
-                  Editar Email
-                </button>
-              )}
+              <input
+                type="text"
+                value={userData.email}
+                disabled
+                className="p-2 w-full bg-gray-700 rounded outline-none "
+              />
             </div>
           </div>
         </div>
