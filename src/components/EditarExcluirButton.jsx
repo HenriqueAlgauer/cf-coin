274b64589d770b518c3029db920ec1a02598d0eb
henriqueAlgauer <algauer.henrique@gmail.com>
2025-02-16 18:09:38 -0300

    estilização responsivo

--- src/components/EditarExcluirButton.jsx
+++ src/components/EditarExcluirButton.jsx
@@ -1,6 +1,6 @@
 function EditarExcluirButton({ editar, exculir }) {
   return (
-    <div className="col-span-2 lg:col-span-1 flex justify-end gap-2">
+    <div className="justify-between mt-2 gap-8 sm:col-span-2 lg:col-span-1 flex sm:justify-end sm:gap-2">
       <button
         className="bg-blue-600 px-3 py-1 cursor-pointer rounded text-white"
         onClick={editar}
