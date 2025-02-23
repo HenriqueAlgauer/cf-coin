af32b9dac0a0e849c14e8951673641e4b8b3b7f5
henriqueAlgauer <algauer.henrique@gmail.com>
2025-02-23 17:44:54 -0300

    estilização

--- src/components/tabelaExibicao/ListItemText.jsx
+++ src/components/tabelaExibicao/ListItemText.jsx
@@ -1,6 +1,6 @@
 function ListItemText({ title, subtitle, text }) {
   return (
-    <div className="">
+    <div className="w-full md:max-w-[85%]">
       <p className="font-bold ">{title}</p>
       <p className="text-gray-400 font-semibold">{subtitle}</p>
       <p className="text-blue-300 font-bold">{text || ""}</p>
